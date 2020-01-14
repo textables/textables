166ab35ceb8b190f5def9fb374f11830c95c30c1
Jacob Harrington <jharrington4@gmail.com>
2020-01-13 16:33:34 -0800

    wrote test that confims gen text function works

--- bot.js
+++ bot.js
@@ -1,17 +1,22 @@
 const Source = require('./lib/models/Source');
 const Markov = require('markov-chains-text').default;
-const chance = require('chance').Chance();
 
-async function generateText(person){
-  let textStr = '';
-  const text = await Source
-    .find({ fullname: person })
+//THIS DOC NEEDS TO BE EDITED BEFORE TWITTER
+
+async function generateText(firstName){
+  
+  //eventually delete the return
+  return await Source
+  //finding by first name will be an issues if we have more that one person with that name
+  //we tried finding by full name and that didn't work.
+    .findOne({ firstName: firstName })
     .populate('texts')
-    .lean()
     .then(res => {
+      let textStr = '';
+      //console log for testing only
+      console.log(res);
       res.texts.forEach(text => {
         textStr += ' ' + text;
-        
       });
       return textStr;
     })
@@ -20,6 +25,8 @@ async function generateText(person){
       return markov.makeSentence();
     });
 
+  //uncomment below for Twitter
+  //.then the next line
   //T.post('status/update', { status: text });
   
 }
