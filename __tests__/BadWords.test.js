4829d8ba179ece38b68ddc00450fae2c88dee34c
Joseph Tatum <josephtatum@gmail.com>
2020-01-15 13:46:45 -0800

    wrote 4 passing tests for arrayIncludes function to make sure "bad" words cant bet used in tweets

--- __tests__/BadWords.test.js
+++ __tests__/BadWords.test.js
@@ -1,21 +1,26 @@
 require('dotenv').config();
-const connect = require('../lib/utils/connect');
-const mongoose = require('mongoose');
 
-describe('Bad words', () => {
-  beforeAll(() => {
-    connect();
+const { arrayIncludes } = require('../lib/createNewSentence');
+
+describe('arrayInclude function should', () => {
+
+  it('return false if string includes "bad" word (example 1)', () => {
+    const result = arrayIncludes(['dog'], 'i have a dog named spot');
+    expect(result).toEqual(false);
   });
 
-  beforeEach(() => {
-    return mongoose.connection.dropDatabase();
+  it('return false if array includes "bad" word (example 2)', () => {
+    const result = arrayIncludes(['cat', 'dog'], 'i have a dog named spot');
+    expect(result).toEqual(false);
   });
 
-  afterAll(() => {
-    return mongoose.connection.close();
+  it('return false if array includes "bad" word (example 3)', () => {
+    const result = arrayIncludes(['dog', 'cat'], 'i have a dog named spot');
+    expect(result).toEqual(false);
   });
 
-  it('', () => {
-
+  it('return true if array does not include "bad" word', () => {
+    const result = arrayIncludes(['cat'], 'i have a dog named spot');
+    expect(result).toEqual(true);
   });
 });
