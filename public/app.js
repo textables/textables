564e1baa84b6c4dbf5ff76e31592dd35e045f562
Brittany Houtz <brittany.houtz@gmail.com>
2020-01-16 10:16:27 -0800

    added random quote fetch for surprise me

--- public/app.js
+++ public/app.js
@@ -3,21 +3,35 @@ const listOfRadioButtons = document.querySelectorAll('input');
 listOfRadioButtons.forEach(buttonValue => {
   buttonValue.addEventListener('click', (event) => {
     const chosenSource = event.target.value;
-    const queryString = window.location.hash.slice(1);
+    if(chosenSource === 'surprise-me') {
+      fetch('/api/v1/results', {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      })
+        .then(res => res.json())
+        .then(res => console.log(res));
 
-    const searchParams = new URLSearchParams(queryString);
-    searchParams.set('sourceId', chosenSource);
+    } else {
 
-    window.location.hash = searchParams.toString();
+      const queryString = window.location.hash.slice(1);
+
+      const searchParams = new URLSearchParams(queryString);
+      searchParams.set('sourceId', chosenSource);
+
+      window.location.hash = searchParams.toString();
 
 
-    //fetch
-    fetch(`/api/v1/results/${chosenSource}`, {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    })
-      .then(res => res.json());
+      //fetch
+      fetch(`/api/v1/results/${chosenSource}`, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      })
+        .then(res => res.json())
+        .then(res => console.log(res));
+    }
   });
 });
