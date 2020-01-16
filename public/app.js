81e8556ec188be5a8cb8a1b95a30444ffe003e77
Jodi N Kansagor <joedvice@gmail.com>
2020-01-16 11:30:29 -0800

    got text in bubble, need to CSS

--- public/app.js
+++ public/app.js
@@ -1,37 +1,28 @@
+import { fetchRandom, fetchWithSource } from './services/fetch-functions.js';
+
 const listOfRadioButtons = document.querySelectorAll('input');
+const quoteTextArea = document.querySelector('.quote-bubble');
+
 
 listOfRadioButtons.forEach(buttonValue => {
-  buttonValue.addEventListener('click', (event) => {
+  buttonValue.addEventListener('click', async(event) => {
     const chosenSource = event.target.value;
-    if(chosenSource === 'surprise-me') {
-      fetch('/api/v1/results', {
-        method: 'GET',
-        headers: {
-          'Content-Type': 'application/json'
-        }
-      })
-        .then(res => res.json())
-        .then(res => console.log(res));
-
+    if (chosenSource === 'surprise-me') {
+      const data = await fetchRandom();
+      quoteTextArea.textContent = data.tweetText;
     } else {
-
+      
       const queryString = window.location.hash.slice(1);
-
       const searchParams = new URLSearchParams(queryString);
       searchParams.set('sourceId', chosenSource);
-
       window.location.hash = searchParams.toString();
-
-
-      //fetch
-      fetch(`/api/v1/results/${chosenSource}`, {
-        method: 'GET',
-        headers: {
-          'Content-Type': 'application/json'
-        }
-      })
-        .then(res => res.json())
-        .then(res => console.log(res));
+      
+      const data = await fetchWithSource(chosenSource);
+      console.log(data.tweetText);
+      quoteTextArea.textContent = data.tweetText;
     }
   });
+
+
+
 });
