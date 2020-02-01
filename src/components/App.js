115e3020e550775a86f3a9f3b3da1c0aa66bceb2
joelpdurham <joeldur@gmail.com>
2020-01-31 16:24:15 -0800

    init done, header updated, linter updated

---
+++ src/components/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import Header from './common/Header';
+
+export default function App() {
+  return (
+    <React.Fragment>
+      <Header />
+    </React.Fragment>);
+}
---
+++ src/components/common/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function Header() { 
+  return <header>
+    <h1>Markov Twain</h1>
+    <h5>&quot;Some things that some famous people definitely said.&quot;</h5>
+  </header>;
+}
+
+export default Header;
