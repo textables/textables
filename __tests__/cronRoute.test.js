27f7c963b35206a32685ff17e86e9cc99dba0d96
joelpdurham <joeldur@gmail.com>
2020-02-16 18:57:24 -0800

    solved cron test issues!

--- __tests__/cronRoute.test.js
+++ __tests__/cronRoute.test.js
@@ -1,8 +1,12 @@
 require('dotenv').config();
 const request = require('supertest');
 const app = require('../lib/app');
+const connect = require('../lib/utils/connect');
 
 describe('Cron Route Test', () => {
+
+  beforeAll(() => connect());
+
   it('can send a quote to the cron job controller and return complete', () => {
     return request(app)
       .post(`/api/v1/cron/${process.env.TWITTER || 'test'}`)
